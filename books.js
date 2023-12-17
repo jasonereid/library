bf93920d29527139cc5fde734169389d565a8056
Jason Reid <94079137+jasonereid@users.noreply.github.com>
2023-12-16 20:56:12 -0600

    Add files via upload

---
+++ books.js
@@ -0,0 +1,8 @@
+function showBookInput() {
+    var x = document.getElementById("bookInputFormDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+};
